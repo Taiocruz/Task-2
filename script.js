721170cca64eb4995f36a8660ea94b39d986f6b6
Taiocruz <35631575+Taiocruz@users.noreply.github.com>
2018-02-10 23:08:31 +0200

    Create script.js

---
+++ script.js
@@ -0,0 +1,76 @@
+//Показ и скртие элемента
+function showHide(){
+	var obj = document.getElementById("sidebar");
+
+	//Если css-свойство display none, то:
+
+	if (obj.style.display === "none") {
+        //Показывваем элемент
+        obj.style.left = 300;
+        obj.style.display = "block";
+		var left = 300;
+        var timer = setInterval(function() {
+            if (obj.style.left === '0px') {
+                clearInterval(timer);
+                document.getElementById('burger').value='Hide Sidebar';
+            } else {
+                left -= 1;
+                obj.style.left = left + 'px';
+            }
+        }, 1);
+	} else {
+        //Скрываем элемент
+        var left = 0;
+        var timer = setInterval(function() {
+            if (obj.style.left === '300px') {
+                clearInterval(timer);
+                obj.style.display = "none";
+                document.getElementById('burger').value='Show Sidebar';
+            } else {
+                left += 1;
+                obj.style.left = left + 'px';
+            }
+        }, 1);
+	}
+}
+        
+//Функция изменения размера шрифта
+function fontS(val){
+    if (val > 24) {
+        document.getElementById("fontSize").value = 24;
+        val = 24;
+    }
+
+    if (val < 8) {
+        document.getElementById("fontSize").value = 8;
+        val = 8
+    }
+	document.getElementById("controledFont").style.fontSize=val+"px";
+}	
+        
+
+//Функция удаления последнего p
+function removeLastP() {
+    var parent=document.getElementById("controledFont");
+    var children=document.getElementsByTagName("p");
+    parent.removeChild(children[children.length - 1]);
+}
+
+
+
+//Изменение размера цвета фона
+
+document.addEventListener("DOMContentLoaded", function() {
+    document.getElementById("colorPeak").addEventListener("change", function(){
+        var section = document.getElementsByClassName("section");
+        section[0].style.background = this.value;
+    });
+	
+//Изменение шрифта
+    var radio = document.getElementsByName("font-style");
+    radio.forEach(function(item) {
+        item.addEventListener("change", function(){
+            document.body.style.fontFamily = this.value;
+        });
+    });
+});
